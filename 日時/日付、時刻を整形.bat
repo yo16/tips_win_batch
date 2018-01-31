f6bab60ee08db5f7daf47332fbc49262b91056fe
yo16 <xxyo1xx@gmail.com>
2018-02-01 08:43:28 +0900

    2/1

--- 日時/日付、時刻を整形.bat
+++ 日時/日付、時刻を整形.bat
@@ -1,6 +1,15 @@
 
+rem %DATE%、%TIME%の書式から、数字の羅列へ変換する
+rem コントロールパネルの地域と言語のオプションで、
+rem 元の書式を変更することができるので
+rem 一般的に使う場合は要注意。
+rem yyyy/mm/dd (ddd) とか yyyy-mm-dd とか。
+rem 
+rem 2018 (c) yo16
+
 set DT=%DATE%
 set DT=%DT:/=%
+set DT=%DT:-=%
 set DT=%DT::=%
 set DT=%DT: =0%
 
@@ -14,3 +23,4 @@ set TM=%TM:~0,6%
 echo %TM%
 
 pause
+
